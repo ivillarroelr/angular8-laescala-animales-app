7fbbd55614a9370499a3058046205973288283c0
Ivan Villarroel <ivillarroel@farmaciasknop.cl>
2020-08-05 17:26:56 -0400

    Se generan componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AnimalComponent } from './components/animal/animal.component';
+import { AnimalTarjetaComponent } from './components/animal-tarjeta/animal-tarjeta.component';
+import { AnimalesComponent } from './components/animales/animales.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AnimalComponent,
+    AnimalTarjetaComponent,
+    AnimalesComponent,
+    BuscadorComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/animales/animales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animales',
+  templateUrl: './animales.component.html',
+  styles: []
+})
+export class AnimalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
